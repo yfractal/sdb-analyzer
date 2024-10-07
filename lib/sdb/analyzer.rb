e80dde7460f3514b7b9961cff89c845f6318d65c
Mike Yang <yfractal@gmail.com>
2024-10-07 13:45:26 +0800

    feat: generate trace

--- lib/sdb/analyzer.rb
+++ lib/sdb/analyzer.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "analyzer/version"
+require_relative "analyzer/exporter"
 require_relative "analyzer/frame_walker"
 require_relative "analyzer/reader"
 require_relative "analyzer/helper"
