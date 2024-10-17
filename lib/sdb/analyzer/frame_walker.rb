006dfa09bc5e05442b3c8f28079bbafa143e92ba
Mike Yang <yfractal@gmail.com>
2024-10-17 23:21:49 +0800

    feat: remove unknow

--- lib/sdb/analyzer/frame_walker.rb
+++ lib/sdb/analyzer/frame_walker.rb
@@ -8,7 +8,7 @@ module Sdb
     class Frame
       attr_writer :parent
       attr_reader :iseq, :children, :duration
-  
+
       def initialize(iseq, ts)
         @iseq     = iseq
         @ts       = ts
@@ -16,15 +16,15 @@ module Sdb
         @children = []
         @parent   = nil
       end
-  
+
       def add_child(frame)
         @children << frame
       end
-  
+
       def update_ts(new_ts)
         @duration = new_ts - @ts
       end
-    end 
+    end
 
     class Walker
       def initialize(log_file, iseq_file)
@@ -34,35 +34,34 @@ module Sdb
         @roots = []
         @stack = []
       end
-  
+
       def draw(name)
         graph = GraphViz.new( :G, :type => :digraph )
         graph[:bgcolor] = '#253238'
         @fake_generation = 0
         @total = @roots.map {|root| root.duration }.sum.to_f
-  
+
         @roots.each do |frame|
           draw_frame(graph, frame)
         end
-  
+
         graph.output( :png => name )
       end
-  
+
       def draw_frame(graph, frame)
         method, file, line_no = @methods_table[frame.iseq]
-
         if file == nil
-          method, file, line_no = 'unknow', 'unknow', 'unknow'
+          method, file, line_no = frame.iseq.to_s, frame.iseq.to_s, frame.iseq.to_s
         end
-  
+
         if file.include?("/")
           label = "#{method}(#{file.split("/")[-3..-1].join("/")}:#{line_no})"
         else
           label = "#{method}(#{file}:#{line_no})"
         end
-  
+
         node = graph.add_nodes(frame.iseq.to_s + "-#{@fake_generation}", label: label, color: '#2e95d3', fontcolor: '#2e95d3')
-  
+
         @fake_generation += 1
         frame.children.each do |child|
           duration = child.duration
@@ -71,10 +70,10 @@ module Sdb
           child = draw_frame(graph, child)
           graph.add_edges(node, child, label: label, color: '#00a67d', fontcolor: '#00a67d') if child
         end
-  
+
         node
       end
-  
+
       def walk(target_trace_id)
         File.new(@log_file).each_line do |line|
           next if line.include?("[methods]")
@@ -88,55 +87,55 @@ module Sdb
 
           ts = data[1]
           iseqs = data[2..-1].reverse # root to deeptest
-  
+
           frame_diff_count = @stack.count - iseqs.count
-  
+
           while frame_diff_count > 0
             @stack.pop
             frame_diff_count -= 1
           end
-  
+
           i = 0
-  
+
           iseqs.each do |iseq|
             if on_stack?(iseq, i)
               update_ts(i, ts)
             else
               on_stack(iseq, ts, i)
             end
-  
+
             i += 1
           end
         end
       end
-  
+
       private
       def update_ts(i, ts)
         frame = @stack[i]
         frame.update_ts(ts)
       end
-  
+
       def on_stack?(iseq, i)
         @stack[i] && @stack[i].iseq == iseq
       end
-  
+
       def on_stack(iseq, ts, i)
         frame = Frame.new(iseq, ts)
         if i == 0
           @roots << frame
         end
-  
+
         @stack[i] = frame
-  
+
         if i != 0
           pre_frame = @stack[i-1]
           frame.parent = pre_frame
           pre_frame.add_child(frame)
         end
       end
-  
+
       def read_methods
-    
+
         iseq_to_method = {}
 
         File.new(@iseq_file).each_line do |line|
