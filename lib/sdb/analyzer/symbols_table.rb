d5808b03bee08b80e5045f17e905eeb616d19e85
Mike Yang <yfractal@gmail.com>
2025-02-11 13:54:10 +0800

    feat: add iseq func to SymbolsTable

--- lib/sdb/analyzer/symbols_table.rb
+++ lib/sdb/analyzer/symbols_table.rb
@@ -8,8 +8,24 @@ module Sdb
         @methods_move_events = [] # for debugging
       end
 
+      def iseq(addr, ts)
+        iseqs = @iseq_to_method[addr]
+        return if iseqs.nil? || iseqs.empty?
+
+        i = 0
+        while i < iseqs.size
+          if i == iseqs.size - 1
+            return iseqs[i]
+          elsif iseqs[i][-1] <= ts && iseqs[i + 1][-1] > ts
+            return iseqs[i]
+          end
+
+          i += 1
+        end
+      end
+
       def read
-        iseq_to_method = {}
+        @iseq_to_method = {}
         cfunc_first_event = nil
 
         module_addr_to_name = {}
@@ -37,34 +53,39 @@ module Sdb
               addr = data['iseq_addr']
 
               ts = data['ts']
-              iseq_to_method[addr] ||= {}
-              iseq_to_method[addr][ts] = cfunc_first_event['name'], module_name, cfunc_first_event['first_lineno'], cfunc_first_event['type'], data['type'], data['ts']
-              iseq_to_method[addr] = iseq_to_method[addr].sort.to_h
+              @iseq_to_method[addr] ||= []
+              @iseq_to_method[addr] << [cfunc_first_event['name'], module_name, cfunc_first_event['first_lineno'], cfunc_first_event['type'], data['type'], data['ts']]
+              @iseq_to_method[addr].sort_by! { |iseq| iseq[-1] }
               cfunc_first_event = nil
             end
           elsif data['type'] != 7 # move event
             addr = data['iseq_addr']
+            # TODO fix me
+            if addr == 0
+              puts "addr is 0, data=#{data}"
+              next
+            end
+
             ts = data['ts']
 
-            iseq_to_method[addr] ||= {}
-            iseq_to_method[addr][ts] = data['name'], data['path'], data['first_lineno'], data['type'], data['type'], ts
-            iseq_to_method[addr] = iseq_to_method[addr].sort.to_h
+            @iseq_to_method[addr] ||= []
+            @iseq_to_method[addr] << [data['name'], data['path'], data['first_lineno'], data['type'], data['type'], ts]
+            @iseq_to_method[addr].sort_by! { |iseq| iseq[-1] }
           else
             from_addr, to_addr, ts = data['iseq_addr'], data['to_addr'], data['ts']
-            if iseq_to_method[from_addr]
+            if @iseq_to_method[from_addr]
               # it moves the most recent record
-              _, iseq = iseq_to_method[from_addr].to_a[-1]
-              new_iseq = iseq.clone
+              new_iseq = @iseq_to_method[from_addr][-1].clone
               new_iseq[-1] = ts
-              iseq_to_method[to_addr] ||= {}
-              iseq_to_method[to_addr][ts] = new_iseq
-              iseq_to_method[to_addr] = iseq_to_method[to_addr].sort.to_h
+              @iseq_to_method[to_addr] ||= []
+              @iseq_to_method[to_addr] << new_iseq
+              @iseq_to_method[to_addr].sort_by! { |iseq| iseq[-1] }
               @methods_move_events << data
             end
           end
         end
 
-        iseq_to_method
+        @iseq_to_method
       end
     end
   end
--- spec/sdb/symbols_table_spec.rb
+++ spec/sdb/symbols_table_spec.rb
@@ -1,22 +1,36 @@
 RSpec.describe Sdb::Analyzer::SymbolsTable do
-  describe '#read' do
-    before do
-      @symbols_table = described_class.new('./spec/data/symbols_table.log')
-      @symbols = @symbols_table.read
-    end
+  before do
+    @symbols_table = described_class.new('./spec/data/symbols_table.log')
+    @symbols = @symbols_table.read
+  end
 
+  describe '#read' do
     it 'reads symbols' do
-      expect(@symbols[281473520570960]).to eq({8126901797125 => ["fffffff", "some-path", 35, 0, 0, 8126901797125]})
+      expect(@symbols[281473520570960][0]).to eq(["fffffff", "some-path", 35, 0, 0, 8126901797125])
     end
 
     it 'reads move events' do
       move_events = @symbols_table.instance_variable_get("@methods_move_events")
       move_event = move_events[0]
       from_addr, to_addr = move_event['iseq_addr'], move_event['to_addr']
-      old_iseq = @symbols[from_addr].values.first
-      new_iseq = @symbols[to_addr].values.first
+      old_iseq = @symbols[from_addr][0]
+      new_iseq = @symbols[to_addr][0]
 
       expect(old_iseq[0...-1]).to eq(new_iseq[0...-1])
+    end
+  end
+
+  describe '#iseq' do
+    it 'reads iseq' do
+      # addr = 281472935568280
+      # [["<top (required)>", "/root/.rbenv/versions/3.1.5/lib/ruby/3.1.0/rubygems/errors.rb", 0, 0, 0, 8126494524917], ["sanitize_string", "/root/.rbenv/versions/3.1.5/lib/ruby/3.1.0/rubygems/specification.rb", 1496, 0, 0, 8126504728750]]
+      iseq = @symbols_table.iseq(281472935568280, 8126494524918)
+      expect(iseq).to eq ["<top (required)>", "/root/.rbenv/versions/3.1.5/lib/ruby/3.1.0/rubygems/errors.rb", 0, 0, 0, 8126494524917]
+    end
+
+    it 'returns 0 if no such addr' do
+      iseq = @symbols_table.iseq(0, 0)
+      expect(iseq).to eq nil
     end
   end
 end
