1292cf35c8a0323edfa43de4022b69a3ee6efb93
Mike Yang <yfractal@gmail.com>
2025-04-05 11:51:37 +0800

    feat: remove uesless files

--- lib/sdb/analyzer.rb
+++ lib/sdb/analyzer.rb
@@ -1,13 +1,9 @@
 # frozen_string_literal: true
 
 require_relative "analyzer/version"
-require_relative "analyzer/exporter"
 require_relative "analyzer/frame_reader"
-require_relative "analyzer/frame_walker"
 require_relative "analyzer/frame_analyzer"
 require_relative "analyzer/puma_log_analyzer"
-require_relative "analyzer/reader"
-require_relative "analyzer/helper"
 require_relative "analyzer/symbols_table"
 require_relative "analyzer/time_converter"
 require_relative "analyzer/symbolizer"
